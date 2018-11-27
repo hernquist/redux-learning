1e550370bbb838797c5d94533caa2e2a9397085a
David Hernquist <david_hernquist@gmail.com>
2018-11-27 16:39:19 -0500

    updated counter.test

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -1,6 +1,9 @@
+import { INCREMENT_COUNTER, ADD } from './constants';
+import { incrementCounter, add } from './actions';
+
 describe('incrementCounter', () => {
   it(`returns INCREMENT_COUNTER action type with no payload`, () => {
-    expect(increment()).toEqual({
+    expect(incrementCounter()).toEqual({
       type: INCREMENT_COUNTER,
     })
   })
@@ -9,8 +12,8 @@ describe('incrementCounter', () => {
 describe('add', () => {
   it(`returns ADD action type with a value payload`, () => {
     const value = 4;
-    expect(increment(value)).toEqual({
-      type: INCREMENT_COUNTER,
+    expect(add(value)).toEqual({
+      type: ADD,
       value
     })
   })
