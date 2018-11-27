440c2e19b5a00ac3b2aabfaa7299dfe548943a14
David Hernquist <david_hernquist@gmail.com>
2018-11-27 16:31:06 -0500

    reducer tests written

---
+++ src/store/reducers/counter.test.js
@@ -0,0 +1,35 @@
+import reducer, { initialState } from './counter';
+import { INCREMENT_COUNTER } from '../constants';
+
+const testState = {
+  counter: 99
+}
+
+describe('INCREMENT_COUNTER', () => {
+  it('increases the initial state counter value by one', () => {
+    const action = { type: INCREMENT_COUNTER }
+    const state = reducer(initialState, action)
+    expect(state.counter).toBe(1);
+  })
+
+  it('increases the counter stored in state by one', () => {
+    const action = { type: INCREMENT_COUNTER }
+    const state = reducer(testState, action)
+    expect(state.counter).toBe(100);
+  })
+})
+
+describe('ADD', () => {
+  it(`increases the initial state counter value by the incoming action's value`, () => {
+    const action = { type: ADD, value: 34 }
+    const state = reducer(initialState, action)
+    expect(state.counter).toBe(43);
+  })
+
+  it(`decreases the counter stored in state by the incoming action's value`, () => {
+    const action = { type: ADD, value: -90 }
+    expect(testState.counter).toBe(99)
+    const state = reducer(testState, action)
+    expect(state.counter).toBe(9);
+  })
+})
